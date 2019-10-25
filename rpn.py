786fba68a013f8527e19a39227630b3c4ad059ee
David Niemann <niedavid@umich.edu>
2019-10-25 13:58:05 -0400

    Initial commit, working test harness

---
+++ rpn.py
@@ -0,0 +1,15 @@
+#usr/bin/env python3
+# python doesn't complie, but rather interprets the commands in the file at runtime
+
+def calculate(arg):
+    pass
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+
+if __name__ == '__main__':
+    main()
+
+

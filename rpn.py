c70ae961fde150cbfa317299e62e8e037637b526
David Niemann <niedavid@umich.edu>
2019-10-25 14:35:43 -0400

    Bare minimum to pass first tests

--- rpn.py
+++ rpn.py
@@ -2,7 +2,19 @@
 # python doesn't complie, but rather interprets the commands in the file at runtime
 
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        #print(token)
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token));
+
+        print(stack)
+    return stack.pop()
 
 def main():
     while True:

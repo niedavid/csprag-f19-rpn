bd3e879b709585b273101675f3efd81509a50d1d
David Niemann <niedavid@umich.edu>
2019-11-06 20:22:21 -0500

    Prepping for HW 8

--- test_rpn.py
+++ test_rpn.py
@@ -1,6 +1,7 @@
 import unittest
 import rpn
 
+
 class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
@@ -13,10 +14,10 @@ class TestBasics(unittest.TestCase):
         self.assertEqual(9, result)
     def test_div1(self):
         result = rpn.calculate('12 8 /')
-        self.assertEqul(1, result)
+        self.assertEqual(1, result)
     def test_div2(self):
         result = rpn.calculate('24 8 /')
-        self.assertEqul(3, result)
+        self.assertEqual(3, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
